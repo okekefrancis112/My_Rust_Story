1ad5e0b6c61ec5e5f04642bd15d4db81bb5d547e
okekefrancis112 <okeke98@gmail.com>
2023-08-18 08:37:21 +0100

    ...

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input);
+    println!("Hello, world!");
+    let mars_weight = calculate_weight_on_mars(100.0);
+    println!("Weight on Mars: {}Kg", mars_weight);
+
+    calculate_weight_on_mars(100.0);
+}
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    (weight / 9.81) * 3.711
+}
