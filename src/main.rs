967a0be8a0e97eb189dcef9e08a03b1beb9e8cc0
okekefrancis112 <okeke98@gmail.com>
2023-08-26 23:51:18 +0100

    ...

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,6 @@ fn calculate_weight_on_mars(weight: f32) -> f32 {
     (weight / 9.81) * 3.711
 }
 
-fn calculate_weight_on_mar(weight: f32) -> f32 {
+fn calculate_weight_on_mars(weight: f32) -> f32 {
     (weight / 9.81) * 3.711
 }
