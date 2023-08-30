ffe4dabe2ae01e7b1b3bf30ca550b979c7aa1ab9
okekefrancis112 <okeke98@gmail.com>
2023-08-30 22:04:06 +0100

    finish

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::io;
 
 fn main() {
+    // println!("Hello, world!");
+    println!("Enter your weight (Kg): ");
     let mut input = String::new();
-    io::stdin().read_line(&mut input);
-    println!("Hello, world!");
-    let mars_weight = calculate_weight_on_mars(100.0);
+    io::stdin().read_line(&mut input).unwrap();
+
+    let weight: f32  = input.trim().parse().unwrap();
+    let mars_weight = calculate_weight_on_mars(weight);
     println!("Weight on Mars: {}Kg", mars_weight);
 
     calculate_weight_on_mars(100.0);
@@ -14,6 +17,9 @@ fn calculate_weight_on_mars(weight: f32) -> f32 {
     (weight / 9.81) * 3.711
 }
 
-fn calculate_weight_on_mars(weight: f32) -> f32 {
-    (weight / 9.81) * 3.711
-}
+//  Ownership in rust
+// 1. each value in rust is owned by a variable
+
+// 2. when the owner goes out of scope, the value will be deallocated
+
+// 3. There can only be one owner at a time
